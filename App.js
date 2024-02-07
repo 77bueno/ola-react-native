af060a8b3255fb701133ca97e4bee06ab7fb668e
victor <victorhugomotabueno@gmail.com>
2024-02-07 11:12:00 -0300

    Layout com Flex

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar, View, Text, SafeAreaView, Button, Alert } from "react-native";
+import { StatusBar, View, Text, SafeAreaView, Button, Alert, StyleSheet } from "react-native";
 
 export default function App() {
   function vai() {
@@ -6,22 +6,45 @@ export default function App() {
   }
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="red" />
-      <SafeAreaView>
-        <View>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.cabecalho}>
           <Text> Topo/Cabeçalho </Text>
         </View>
 
-        <View>
+        <View  style={estilos.conteudo}>
           <Text> Conteúdo </Text>
         </View>
 
-        <View>
+        <View style={estilos.rodape}>
           <Text> Rodapé </Text>
-          <Button title="Toca aqui" color="red" onPress={vai} />
+          <Button title="Toca aqui" color="darkblue" onPress={vai} />
         </View>
       </SafeAreaView>
       
     </>
   )
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "#E4F7FF",
+    flex: 1,
+  },
+  cabecalho: {
+    backgroundColor: "#6BD3FF",
+    flex: 0.5,
+
+  }, 
+  conteudo: {
+    backgroundColor: "#51CBFF",
+    flex: 4,
+
+  }, 
+  rodape: {
+    backgroundColor: "#34C2FF",
+    flex: 0.5,
+
+  }
+
+});
