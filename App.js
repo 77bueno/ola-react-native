7f74e26e649de65f65e30c1681d31c14d1b55e2e
victor <victorhugomotabueno@gmail.com>
2024-02-07 10:32:19 -0300

    Marcação de Texto

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar, View, Text } from "react-native";
+import { StatusBar, View, Text, SafeAreaView, Button, Alert } from "react-native";
 
 export default function App() {
+  function vai() {
+    Alert.alert("Olá! ", "Fulano!");
+  }
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="red" />
-      <View>
-        <Text> React Native - Emulador </Text>
-      </View>
+      <SafeAreaView>
+        <View>
+          <Text> Topo/Cabeçalho </Text>
+        </View>
+
+        <View>
+          <Text> Conteúdo </Text>
+        </View>
+
+        <View>
+          <Text> Rodapé </Text>
+          <Button title="Toca aqui" color="red" onPress={vai} />
+        </View>
+      </SafeAreaView>
+      
     </>
   )
 }
